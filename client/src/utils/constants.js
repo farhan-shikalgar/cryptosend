bf21b30c89a90485d5cd2302ebd81f0bc8268eee
farhan-shikalgar <farhanshikalgar@gmail.com>
2023-07-22 10:33:21 +0530

    first commit

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from "./Transactions.json";
+
+export const contractAddress = "0x1EF753F0Ee21152b0D9151a8866E562a3475D02C";
+export const contractABI = abi.abi;
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,15 @@
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    sepolia: {
+      url: process.env.ALCHEMY_API_KEY,
+      accounts: [process.env.GOERLI_PRIVATE_KEY],
+    },
+  },
+};
